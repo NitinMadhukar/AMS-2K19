20e00796ea9ed96d51518130d97a90c9a1a941b2
r00tDada <sourabh18299@gmail.com>
2019-07-05 05:09:35 +0530

    AMS-2k19

--- js/theme.js
+++ js/theme.js
@@ -290,21 +290,3 @@ $(document).ready(function () {
 
 });
 
-
-
-// Youtube
-
-$(document).on('click','.js-videoPoster',function(e) {
-  e.preventDefault();
-  var poster = $(this);
-  var wrapper = poster.closest('.js-videoWrapper');
-  videoPlay(wrapper);
-});
-
-
-function videoPlay(wrapper) {
-  var iframe = wrapper.find('.js-videoIframe');
-  var src = iframe.data('src');
-  wrapper.addClass('videoWrapperActive');
-  iframe.attr('src',src);
-}
