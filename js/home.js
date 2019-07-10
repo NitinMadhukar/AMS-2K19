9aa7f7c204f842ad3ac86e566c33f9b1028b9c7c
Nitin Madhukar <iec2018053@iiita.ac.in>
2019-07-11 02:17:36 +0530

    AMS-2K19

--- js/home.js
+++ js/home.js
@@ -36,7 +36,7 @@
 var canvasDiv = document.getElementById('particle-canvas');
 var options = {
     particleColor: '#888',
-    background: 'https://raw.githubusercontent.com/JulianLaval/canvas-particle-network/master/img/demo-bg.jpg',
+    background: 'https://raw.githubusercontent.com/NitinMadhukar/AMS-2K19/master/images/home/front.jpg',
     interactive: true,
     speed: 'medium',
     density: 'high',
